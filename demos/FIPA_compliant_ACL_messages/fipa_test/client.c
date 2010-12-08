dc8b4a7790fc663017a8dbb6221c771cadab3a67
David Ko <david.dk.ko@gmail.com>
2010-12-08 14:44:15 -0800

    Updated demo for auto-testing

--- demos/FIPA_compliant_ACL_messages/fipa_test/client.c
+++ demos/FIPA_compliant_ACL_messages/fipa_test/client.c
@@ -19,8 +19,12 @@ int main()
   /* Note: The third argument of the following function may also be a
      valid IP address in the form of a string. i.e. 192.168.0.1 */
   MC_SendAgentFile(agency, "test1.xml");
-  printf("Press 'enter' to send the next agent.\n");
-  getchar();
+  printf("Sending next agent in 3 seconds...\n");
+#ifndef _WIN32
+  sleep(3);
+#else
+  Sleep(3000);
+#endif
   MC_SendAgentFile(agency, "test2.xml");
   MC_End(agency);
   exit(0);
