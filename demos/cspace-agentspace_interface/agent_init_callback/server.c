c06f44b751cac4a6b3078592aecc529c0d2c3269
David Ko <david.dk.ko@gmail.com>
2010-12-28 13:28:49 -0800

    added agent init callback demo

--- demos/cspace-agentspace_interface/agent_init_callback/server.c
+++ demos/cspace-agentspace_interface/agent_init_callback/server.c
@@ -2,6 +2,10 @@
 
 #include <stdio.h>
 #include <libmc.h>
+#include <embedch.h>
+
+int agentCallbackFunc(ChInterp_t interp, MCAgent_t agent, void* user_data);
+EXPORTCH double mult_chdl(void* varg);
 
 int main() 
 {
@@ -10,9 +14,34 @@ int main()
   setbuf(stdout, NULL);
   
   agency = MC_Initialize(local_port, NULL);
+  MC_AddAgentInitCallback(agency, agentCallbackFunc, NULL);
 
   MC_MainLoop(agency);
 
   MC_End(agency);
   return 0;
+}
+
+/* This callback function is called during the initialization step of each
+ * incoming agent. We will add a c-space function to each interpreter that
+ * the agent will be able to call. */
+int agentCallbackFunc(ChInterp_t interp, MCAgent_t agent, void* user_data)
+{
+  Ch_DeclareFunc(interp, "double mult(double x, double y);", (ChFuncdl_t)mult_chdl);
+  return 0; /* 0 for success error status */
+}
+
+EXPORTCH double mult_chdl(void* varg)
+{
+  double retval;
+  double x, y;
+  ChInterp_t interp;
+  ChVaList_t ap;
+
+  Ch_VaStart(interp, ap, varg);
+  x = Ch_VaArg(interp, ap, double);
+  y = Ch_VaArg(interp, ap, double);
+  retval = x * y;
+  Ch_VaEnd(interp, ap);
+  return retval;
 }
